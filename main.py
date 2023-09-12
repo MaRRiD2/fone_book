95685452b08070ae706498f9f4766cfe001b47b0
Maksim <norten1156@gmail.com>
2023-09-12 11:58:58 +0300

    give void

--- main.py
+++ main.py
@@ -5,3 +5,30 @@ def phone_format(n):  # форматирование телефонного но
     n = n.removeprefix("+")
     n = re.sub("[ ()-]", "", n)
     return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
+
+
+def printData(data):  # Функция вывода телефонной книги в консоль
+    phoneBook = []
+    splitLine = "=" * 49
+    print(splitLine)
+    print(" №  Lastname        Name          Phone Numbers")
+    print(splitLine)
+    personID = 1
+
+    for contact in data:
+        lastName, name, phone = contact.rstrip().split(",")
+        phoneBook.append(
+            {
+                "ID": personID,
+                "lastName": lastName,
+                "name": name,
+                "phone": phone_format(phone),
+            }
+        )
+        personID += 1
+
+    for contact in phoneBook:
+        personID, lastName, name, phone = contact.values()
+        print(f"{personID:>2}. {lastName:<15} {name:<10} -- {phone:<15}")
+
+    print(splitLine)
